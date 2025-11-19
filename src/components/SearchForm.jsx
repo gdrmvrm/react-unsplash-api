8f0790496e974fffed10a599d9ed41b3c2f7481c
gdrmvrm <gdrmvrm@gmail.com>
2025-11-19 12:54:07 +0000

    Setup search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const SearchForm = () => {
-  return <div>search away</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input className="form-input search-input" type="text" name="search" placeholder="cat"></input>
+        <button className="btn" type="submit">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
