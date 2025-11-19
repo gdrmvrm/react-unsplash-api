5c2b48142c4a3089681173818c391a7bc268a2cb
gdrmvrm <gdrmvrm@gmail.com>
2025-11-19 10:41:07 +0000

    DarkTheme initial set up

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useGlobalContext } from '../context';
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
-const Toggle = () => {
-  const { greeting } = useGlobalContext();
-  return <h2>{`${greeting} Toggle World`}</h2>;
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? <BsFillSunFill className="toggle-icon" /> : <BsFillMoonFill className="toggle-icon" />}
+      </button>
+    </section>
+  );
 };
 
-export default Toggle;
+export default ThemeToggle;
